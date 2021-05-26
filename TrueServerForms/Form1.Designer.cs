acc6e8150668a7dafb5d119fec3ebc306b32a6ba
Felicia Stenvall <felicia.stenvall2244@gmail.com>
2021-05-26 09:30:10 +0200

    Add project files.

---
+++ TrueServerForms/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace TrueServerForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbxInbox = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(253, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Server";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbxInbox);
+            this.groupBox1.Location = new System.Drawing.Point(22, 122);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(450, 242);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Inbox";
+            // 
+            // tbxInbox
+            // 
+            this.tbxInbox.Location = new System.Drawing.Point(10, 46);
+            this.tbxInbox.Name = "tbxInbox";
+            this.tbxInbox.Size = new System.Drawing.Size(426, 183);
+            this.tbxInbox.TabIndex = 0;
+            this.tbxInbox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 388);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox tbxInbox;
+    }
+}
+
---
+++ TrueServerForms/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TrueServerForms
+{
+    public partial class Form1 : Form
+    {
+        TcpListener listener;
+        TcpClient   klient;
+        int         port = 12345;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+            klient = listener.AcceptTcpClient();
+
+            byte[] inData = new byte[256];
+            int antalByte = klient.GetStream().Read(inData, 0, inData.Length);
+
+            tbxInbox.Text = Encoding.Unicode.GetString(inData, 0, antalByte);
+            klient.Close();
+            listener.Stop();
+
+        }
+    }
+}
